df09c2a6f6b526249246f1c1b70332e02691831e
Siamion <sem_en264@mail.ru>
2019-04-15 16:50:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using WcfServiceContract;
+
+namespace WebServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:10004/ServiceWCF");
+            ServiceHost myHost = new ServiceHost(typeof(MyCalculator), baseAddress);
+            try
+            {
+                WSHttpBinding myBinding = new WSHttpBinding();
+                myHost.AddServiceEndpoint(typeof(ICalculator), myBinding, "endpoint1");
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                myHost.Description.Behaviors.Add(smb);
+                myHost.Open();
+                Console.WriteLine("Service Started");
+                Console.WriteLine("Press <ENTER> to dismiss");
+                Console.WriteLine();
+                Console.ReadLine();
+                myHost.Close();
+            }
+            catch(CommunicationException ce)
+            {
+                Console.WriteLine("Exception occured: {0}", ce.Message);
+                myHost.Abort();
+            }
+        }
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceContract
+{
+    public class MyCalculator : ICalculator
+    {
+        double sum = 0;
+
+        public double Add(double n1, double n2)
+        {
+            double result = n1 + n2;
+            Console.WriteLine("Add of {0} and {1} is {2}", n1, n2, result);
+            return result;
+        }
+
+        public double Subt(double n1, double n2)
+        {
+            double result = n1 - n2;
+            Console.WriteLine("Subt of {0} and {1} is {2}", n1, n2, result);
+            return result;
+        }
+
+        public double Multiply(double n1, double n2)
+        {
+            double result = n1 + n2;
+            Console.WriteLine("Multiply of {0} and {1} is {2}", n1, n2, result);
+            return n1 * n2;
+        }
+
+        public double Sum(double n1)
+        {
+            double result = sum + n1;
+            Console.WriteLine("Sum of {0} is {1}", n1, result);
+            return result;
+        }
+    }
+}
